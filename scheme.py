7e604b83868e3c82f1f37942fb36f555678e3cf9
Marvin Killing <marvinkilling@gmail.com>
2011-07-01 00:51:24 -0700

    almost there

--- scheme.py
+++ scheme.py
@@ -1,3 +1,52 @@
+def exp_as_list(exp):
+	ret = []
+	def get_first(exp):
+		exp = exp.strip()
+		if exp.startswith('('):
+			ct = 1
+			idx = 1
+			while ct > 0:
+				if exp[idx] == '(':
+					ct += 1
+				elif exp[idx] == ')':
+					ct -= 1
+				idx += 1
+			return exp[:idx], exp[idx:]
+		else:
+			first_space = exp.find(' ')
+			if first_space > 0:
+				return exp[:first_space], exp[first_space+1:]
+			else:
+				return exp, ""
+	if exp.startswith('(') and exp.endswith(')'):
+		exp = exp[1:-1]
+	while len(exp) > 0:
+		first, exp = get_first(exp)
+		print first, exp
+		ret.append(first)
+	return ret
+
+class Proc:
+	def __init__(self, env):
+		self.env = env
+	def apply(self, args):
+		raise NotImplemented()
+
+class CompoundProcedure(Proc):
+	def __init__(self, exp, env):
+		Proc.__init__(self, env)
+		lst = exp_as_list(exp)
+		self.params = exp_as_list(lst[1])
+		self.body = lst[2:]
+	def apply(self, args):
+		return eval_sequence(self.body, extend_environment(dict.fromkeys(self.params, args), self.env))
+
+class PlusProc(Proc):
+	def __init__(self, env):
+		Proc.__init__(self, env)
+	def apply(self, args):
+		return str(map(float, map(eval, args)).sum())
+
 def lookup_var(name, env):
 	for frame in env:
 		if name in frame:
@@ -33,6 +82,21 @@ def eval_definition(exp, env):
 	parts = exp.strip('()').split(' ')
 	define_var(parts[1], parts[2], env)
 
+def eval_if(exp, env):
+	lst = exp_as_list(exp)
+	if is_true(lst[0], env):
+		return eval(lst[1], env)
+	else:
+		return eval(lst[2], env)
+
+def eval_sequence(exps, env):
+	for exp in exps:
+		lst = eval(exp, env)
+	return lst
+
+def is_true(exp, env):
+	pass
+
 def is_number(s):
 	try:
 		float(s)
@@ -44,31 +108,51 @@ def is_string(s):
 	import re
 	return re.match("\\\"[^\\\"]*\\\"$", s) != None
 
-def self_evaluating(exp):
+def is_self_evaluating(exp):
 	return is_string(exp) or is_number(exp)
 
-def variable(exp):
+def is_variable(exp):
 	return not (exp.startswith("(") or " " in exp)
 	
-def quoted(exp):
+def is_quoted(exp):
 	return exp.startswith("(quote") or exp.startswith("'")
 	
-def definition(exp):
+def is_definition(exp):
 	return exp.startswith("(define")
 
-def assignment(exp):
+def is_assignment(exp):
 	return exp.startswith("(set!")
 
+def is_lambda(exp):
+	return exp.startswith("(lambda")
+
+def is_application(exp):
+	return len(exp_as_list(exp)) > 1
+
 def eval(exp, env):
-	if self_evaluating(exp):
+	if is_self_evaluating(exp):
 		return exp
-	elif variable(exp):
+	elif is_variable(exp):
 		return lookup_var(exp, env)
-	elif quoted(exp):
+	elif is_quoted(exp):
 		return quoted_value(exp)
-	elif assignment(exp):
+	elif is_assignment(exp):
 		return eval_assignment(exp, env)
-	elif definition(exp):
-		 return eval_definition(exp, env)
+	elif is_definition(exp):
+		return eval_definition(exp, env)
+	elif is_if(exp):
+		return eval_if(exp, env)
+	elif is_lambda(exp):
+		return CompoundProcedure(exp, env)
+	elif is_begin(exp):
+		return eval_sequence(exp_as_list(exp)[1:], env)
+	elif is_cond(exp):
+		pass
+	elif is_application(exp):
+		lst = exp_as_list(exp)
+		if lst[0] == '+':
+			return PlusProc(env)
+		else:
+			return eval(lst[0], env).apply(lst[1:])
 	else:
 		raise "unknown expression type " + exp
